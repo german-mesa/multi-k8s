4eb5253f7998d47b2fb5eadda94f566a35eecfdd
German Mesa <german.mesa@sap.com>
2019-05-16 16:43:39 +0200

    Nada

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 
-docker build -t german-mesa/multi-client:latest -t german-mesa/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t german-mesa/multi-server:latest -t german-mesa/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t german-mesa/multi-worker:latest -t german-mesa/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t i028929/multi-client:latest -t i028929/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t i028929/multi-server:latest -t i028929/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t i028929/multi-worker:latest -t i028929/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push german-mesa/multi-client:latest
-docker push german-mesa/multi-server:latest
-docker push german-mesa/multi-worker:latest
+docker push i028929/multi-client:latest
+docker push i028929/multi-server:latest
+docker push i028929/multi-worker:latest
 
-docker push german-mesa/multi-client:$SHA 
-docker push german-mesa/multi-server:$SHA 
-docker push german-mesa/multi-worker:$SHA 
+docker push i028929/multi-client:$SHA 
+docker push i028929/multi-server:$SHA 
+docker push i028929/multi-worker:$SHA 
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=german-mesa/multi-client:$SHA
-kubectl set image deployments/server-deployment server=german-mesa/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=german-mesa/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=i028929/multi-client:$SHA
+kubectl set image deployments/server-deployment server=i028929/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=i028929/multi-worker:$SHA
